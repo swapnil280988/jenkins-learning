454296ca197baaaeb15c8f8b1c415cdabda47306
swapnil280988 <67736734+swapnil280988@users.noreply.github.com>
2024-02-15 20:01:07 +0530

    Change to install docker

--- firstfile.sh
+++ firstfile.sh
@@ -1,7 +1,6 @@
-date
-
-whoami
-
-sudo yum install net-tools -y 
-
-sudo ifconfig
+sudo sh -c 'yum install docker -y'
+sudo sh -c 'systemctl enable docker --now'
+sudo sh -c 'docker run -dit --name myos1 httpd'
+sudo sh -c 'docker ps -a'
+sudo sh -c 'docker exec -it myos1 bash'
+pwd
