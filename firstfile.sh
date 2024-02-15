7aa98551ab0487bec255a92578c3be54741c6afe
swapnil280988 <67736734+swapnil280988@users.noreply.github.com>
2024-02-15 21:18:09 +0530

    removed docker exec command

--- firstfile.sh
+++ firstfile.sh
@@ -2,5 +2,4 @@ sudo sh -c 'yum install docker -y'
 sudo sh -c 'systemctl enable docker --now'
 sudo sh -c 'docker run -dit --name myos1 httpd'
 sudo sh -c 'docker ps -a'
-sudo sh -c 'docker exec -it myos1 bash'
 pwd
